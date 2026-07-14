7190f67aebe41c5483cccd224e32768f2ce5463a
maximayu <mochiokashino@gmail.com>
2026-07-14 22:26:29 +0900

    Implement date and time inputs for availability
    Added date and time input fields for user availability.

--- app.py
+++ app.py
@@ -27,6 +27,21 @@ movie2 = st.text_input(
 st.divider()
 
 # -----------------------------
+# 空き時間
+# -----------------------------
+st.header("📅 空き時間")
+
+date = st.date_input("日付")
+
+start_time = st.time_input(
+    "開始時刻"
+)
+
+end_time = st.time_input(
+    "終了時刻"
+)
+
+# -----------------------------
 # 検索ボタン
 # -----------------------------
 if st.button("🔍 プランを検索"):
@@ -35,3 +50,7 @@ if st.button("🔍 プランを検索"):
 
     st.write("作品①：", movie1)
     st.write("作品②：", movie2)
+
+    st.write("日付：", date)
+    st.write("開始：", start_time)
+    st.write("終了：", end_time)
